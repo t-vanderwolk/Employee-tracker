b0141faa7e2536ba2e450716689d7b12f2be36b1
t-vanderwolk <97120854+t-vanderwolk@users.noreply.github.com>
2022-05-14 13:24:28 -0700

    db connected, promps are working

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 require('dotenv').config();
-const inquirer = require("inquirer");
+const dotenv=require('dotenv');
+const inquirer = require('inquirer');
 const mysql = require('mysql2');
 
-// Connect to the employees_db
-const db = mysql.createConnection(
-    {
-      host: 'localhost',
-      user: 'root',
-      password: process.env,
-      database: 'employees_db'
-    },
-);
+// // Connect to the employees_db
+// const db = mysql.createConnection(
+//     {
+//       host: 'localhost',
+//       user: 'root',
+//       password: process.env.DB_PASS,
+//       database: 'employees_db'
+//     },
+// );
 
 //Inquirer question array
 const categoryQuestions = [
