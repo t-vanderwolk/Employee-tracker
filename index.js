8d6e5c87d7c7d4fade4313eb0056686269487bec
t-vanderwolk <97120854+t-vanderwolk@users.noreply.github.com>
2022-05-15 15:46:55 -0700

    all functions are operating correctly and displaying properly

--- index.js
+++ index.js
@@ -1,152 +1,616 @@
-require('dotenv').config();
-const dotenv=require('dotenv');
-const inquirer = require('inquirer');
-const mysql = require('mysql2');
-const questions = require('./assets/questions.js');
+require( 'dotenv' ).config();
+const inquirer = require( 'inquirer' );
+const mysql = require( 'mysql2' );
+const util = require( 'util' );
+const questions = require( './assets/questions' );
+const queries = require( './assets/queries' );
 
 // Connect to the employees_db
 const db = mysql.createConnection(
-    {
-      host: 'localhost',
-      user: 'root',
-      password: process.env.DB_PASSWORD,
-      database: 'employees_db'
-    },
+	{
+		host: 'localhost',
+		user: 'root',
+		password: process.env.DB_PASSWORD,
+		database: 'employees_db'
+	},
 );
 
 // Turn database queries into a promise
 db.query = util.promisify( db.query );
 
-// View the data on a table with the table name passed in
-const viewAllTable = (table) => {
-    db.query(`SELECT * FROM ${table}`, (err, results) => {
+// Display the table to the console
+const viewTable = ( table ) => {
+	console.log( '\n' );
+	console.table( table );
+	console.log( '\n' );
+};
 
-        if (err) console.log(err);
+// View the data on the 'department' table
+const selectDepartmentTable = async () => {
+	try {
+		const table = await db.query( queries.departments );
 
-        console.log('\n');
-        console.table(results);
-        console.log('\n');
+		viewTable( table );
 
-        return askForCategory();
-    });
-}
+		return askForCategory();
 
+	} catch ( err ) {
+		console.log( err );
+	}
+};
+
+// Sum a department's budget
+const selectDepartmentBudgetTable = async () => {
+	let chooseDepartmentQuestions = [];
+
+	try {
+		const table = await db.query( queries.departments );
+
+		let deptArray = table.map( dept => ( {
+			name: dept.name,
+			value: dept.id
+		} ) );
+
+		chooseDepartmentQuestions.push( constructListQuestion( 'Choose a department to view budget', 'department', deptArray ) );
+
+	} catch ( err ) {
+		console.log( err );
+	}
+
+	inquirer
+		.prompt( chooseDepartmentQuestions )
+		.then( async ( choosenDepartment ) => {
+
+			const department = choosenDepartment.department;
+
+			try {
+				const budgetTable = await db.query( queries.budget, department );
+
+				if( budgetTable[0].department === null ) {
+					console.log( '\x1b[32m', 'This department has no employees.', '\x1b[0m' );
+				} else {
+					viewTable( budgetTable );
+				}
+
+				return askForCategory();
+
+			} catch ( err ) {
+				console.log( err );
+			}
+		} );
+};
 
 // Add a department to database
 const addDepartment = () => {
-    inquirer
-        .prompt(questions.addDepartment)
-        .then((addDepartmentAnswer) => {
+	inquirer
+		.prompt( questions.addDepartment )
+		.then( async ( addDepartmentAnswer ) => {
 
-            const deptName = addDepartmentAnswer.name;
+			const deptName = addDepartmentAnswer.name;
 
-            db.query('INSERT INTO department (name) VALUES (?)', deptName, (err, results) => {
+			try {
+				await db.query( queries.insertDepartment, deptName );
 
-                if (err) console.log(err);
+				console.log( '\x1b[32m', `Added ${deptName} to the database.`, '\x1b[0m' );
 
-                console.log(`Added ${deptName} to the database.`);
+				return askForCategory();
 
-                return askForCategory();
-            });
-        });
-}
+			} catch ( err ) {
+				console.log( err );
+			}
+		} );
+};
 
+// Remove a department
+const deleteDepartment = async () => {
+	let chooseDepartmentQuestions = [];
 
+	try {
+		const table = await db.query( queries.departments );
+
+		let deptArray = table.map( dept => ( {
+			name: dept.name,
+			value: dept.id
+		} ) );
+
+		chooseDepartmentQuestions.push( constructListQuestion( 'Choose a department to delete', 'department', deptArray ) );
+
+	} catch ( err ) {
+		console.log( err );
+	}
+
+	inquirer
+		.prompt( chooseDepartmentQuestions )
+		.then( async ( choosenDepartment ) => {
+
+			const department = choosenDepartment.department;
+
+			try {
+				await db.query( queries.delete( 'department' ), department );
+
+				console.log( '\x1b[33m', 'Deleted department from the database.', '\x1b[0m' );
+
+				return askForCategory();
+
+			} catch ( err ) {
+				console.log( err );
+			}
+		} );
+};
 
 // Ask the user for what action they want to take with departments
-const AskForDepartmentAction = () => {
-    inquirer
-        .prompt(departmentQuestions)
-        .then((departmentAnswer) => {
+const askForDepartmentAction = () => {
+	inquirer
+		.prompt( questions.department )
+		.then( ( departmentAnswer ) => {
 
-            switch(departmentAnswer.action) {
+			switch( departmentAnswer.action ) {
 
-                case "View All Departments":
-                    return viewAllTable('department');
+			case 'View All Departments':
+				return selectDepartmentTable();
 
-                case "Add A Department":
-                    return addDepartment();
-            }
-        });
-}
+			case 'View Department Budget':
+				return selectDepartmentBudgetTable();
 
-// Takes a message, property name, and object array of names to create a list question
-const constructListQuestion = (message, name, objArray) => {
-    let list = [];
+			case 'Add A Department':
+				return addDepartment();
 
-    objArray.forEach(i => list.push(i.name));
+			case 'Delete A Department':
+				return deleteDepartment();
+			}
+		} );
+};
 
-    const question = {
-        type: "list",
-        message: message,
-        name: name,
-        choices: list
-    }
+// Takes a message, property name, and object array to create a list question
+const constructListQuestion = ( message, name, objArray ) => {
+	return {
+		type: 'list',
+		message: message,
+		name: name,
+		choices: objArray
+	};
+};
 
-    return question;
-}
+// View the data on the 'role' table joined with department table
+const selectRoleTable = async () => {
+	try {
+		const table = await db.query( queries.roles );
+
+		viewTable( table );
+
+		return askForCategory();
+
+	} catch ( err ) {
+		console.log( err );
+	}
+};
 
 // Add a role to database
-const addRole = () => {
-    db.query('SELECT name FROM department', (err, results) => {
+const addRole = async () => {
+	try {
+		const deptTable = await db.query( queries.departments );
 
-        addRoleQuestions.push(constructListQuestion("Choose a department for this role", "department", results));
+		let deptArray = deptTable.map( dept => ( {
+			name: dept.name,
+			value: dept.id
+		} ) );
 
-        inquirer
-            .prompt(addRoleQuestions)
-            .then((addRoleAnswers) => {
+		questions.addRole.push( constructListQuestion( 'Choose a department for this role', 'department', deptArray ) );
 
-                const title = addRoleAnswers.title;
-                const salary = addRoleAnswers.salary;
-                const department = addRoleAnswers.department;
+	} catch ( err ) {
+		console.log( err );
+	}
 
-                db.query('SELECT id FROM department WHERE name = ?', department, (err, results) => {
+	inquirer
+		.prompt( questions.addRole )
+		.then( async ( addRoleAnswers ) => {
 
-                    if (err) console.log(err);
+			const { title, salary, department } = addRoleAnswers;
 
-                    const department_id = results[0].id;
+			try {
+				await db.query( queries.insertRole, [title, salary, department] );
 
-                    db.query('INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)', [title, salary, department_id], (err, results) => {                    
+				console.log( '\x1b[32m', `Added ${title} to the database.`, '\x1b[0m' );
 
-                        if (err) console.log(err);
+				return askForCategory();
 
-                        console.log(`Added ${title} to the database.`);
+			} catch ( err ) {
+				console.log( err );
+			}
+		} );
+};
 
-                        return askForCategory();
-                      
-               
-            
-});
+// Remove a role
+const deleteRole = async () => {
+	let chooseRoleQuestions = [];
 
-const addDepartment = () => {
-    inquirer
-       
-        .prompt(questions.addDepartment)
-        .then((addDepartmentAnswer) => {
+	try {
+		const table = await db.query( queries.roles );
 
-            const deptName = addDepartmentAnswer.name;
-const addDepartment = () => {
+		let roleArray = table.map( role => ( {
+			name: role.title,
+			value: role.id
+		} ) );
 
-                if (err) console.log(err);
+		chooseRoleQuestions.push( constructListQuestion( 'Choose a role to delete', 'role', roleArray ) );
 
-                console.log(`Added ${deptName} to the database.`);
-                console.log('\x1b[32m', `Added ${deptName} to the database.`, '\x1b[0m');
+	} catch ( err ) {
+		console.log( err );
+	}
 
-                return askForCategory();
-        
-        };
+	inquirer
+		.prompt( chooseRoleQuestions )
+		.then( async ( choosenRole ) => {
 
-//the user for what action they want to take with roles
-const AskForRoleAction = () => {
-    inquirer
-        .prompt(roleQuestions)
-        .then((roleAnswer) => {
+			const role = choosenRole.role;
 
-            switch(roleAnswer.action) {
+			try {
+				await db.query( queries.delete( 'role' ), role );
 
-                case "View All Roles":
-                    return viewAllTable('role');
+				console.log( '\x1b[33m', 'Deleted role from the database.', '\x1b[0m' );
 
-                case "Add A Role":
-                    return addRole();
-            
+				return askForCategory();
+
+			} catch ( err ) {
+				console.log( err );
+			}
+		} );
+};
+
+// Ask the user for what action they want to take with roles
+const askForRoleAction = () => {
+	inquirer
+		.prompt( questions.role )
+		.then( ( roleAnswer ) => {
+
+			switch( roleAnswer.action ) {
+
+			case 'View All Roles':
+				return selectRoleTable();
+
+			case 'Add A Role':
+				return addRole();
+
+			case 'Delete A Role':
+				return deleteRole();
+			}
+		} );
+};
+
+// View the data on the 'employee' table joined with department and role tables
+const selectEmployeeTable = async () => {
+	try {
+		const table = await db.query( queries.employees() );
+
+		viewTable( table );
+
+		return askForCategory();
+
+	} catch ( err ) {
+		console.log( err );
+	}
+};
+
+// View the data on the 'employee' table by manager joined with department and role tables
+const selectEmployeeManagerTable = async () => {
+	let chooseEmployeeManagerQuestions = [];
+
+	try {
+		const managerTable = await db.query( queries.managers );
+
+		let managerArray = managerTable.map( manager => ( {
+			name: manager.name,
+			value: manager.id
+		} ) );
+
+		chooseEmployeeManagerQuestions.push( constructListQuestion( 'What manager\'s employees would you like to see?', 'manager', managerArray ) );
+
+	} catch ( err ) {
+		console.log( err );
+	}
+
+	inquirer
+		.prompt( chooseEmployeeManagerQuestions )
+		.then( async ( managerChoice ) => {
+
+			const manager = managerChoice.manager;
+
+			try {
+				const table = await db.query( queries.employeesByManager(), manager );
+
+				if( table.length === 0 ) {
+					console.log( '\x1b[32m', 'This manager has no employees.', '\x1b[0m' );
+				} else {
+					viewTable( table );
+				}
+
+				return askForCategory();
+
+			} catch ( err ) {
+				console.log( err );
+			}
+		} );
+};
+
+// View the data on the 'employee' table by department joined with department and role tables
+const selectEmployeeDepartmentTable = async () => {
+	let chooseEmployeeDepartmentQuestions = [];
+
+	try {
+		const deptTable = await db.query( queries.departments );
+
+		let deptArray = deptTable.map( dept => ( {
+			name: dept.name,
+			value: dept.id
+		} ) );
+
+		chooseEmployeeDepartmentQuestions.push( constructListQuestion( 'What department\'s employees would you like to see?', 'department', deptArray ) );
+
+	} catch ( err ) {
+		console.log( err );
+	}
+
+	inquirer
+		.prompt( chooseEmployeeDepartmentQuestions )
+		.then( async ( departmentChoice ) => {
+
+			const department = departmentChoice.department;
+
+			try {
+				const table = await db.query( queries.employeesByDepartment(), department );
+
+				if( table.length === 0 ) {
+					console.log( '\x1b[32m', 'This department has no employees.', '\x1b[0m' );
+				} else {
+					viewTable( table );
+				}
+
+				return askForCategory();
+
+			} catch ( err ) {
+				console.log( err );
+			}
+		} );
+};
+
+// Add an employee to database
+const addEmployee = async () => {
+	try {
+		const roleTable = await db.query( queries.roles );
+
+		let roleArray = roleTable.map( role => ( {
+			name: role.title,
+			value: role.id
+		} ) );
+
+		questions.addEmployee.push( constructListQuestion( 'Choose a role for this employee', 'role', roleArray ) );
+
+	} catch ( err ) {
+		console.log( err );
+	}
+
+	try {
+		const managerTable = await db.query( queries.managers );
+
+		let managerArray = managerTable.map( manager => ( {
+			name: manager.name,
+			value: manager.id
+		} ) );
+
+		managerArray.push( {
+			name: 'No Manager',
+			value: null
+		} );
+
+		questions.addEmployee.push( constructListQuestion( 'Choose a manager for this employee', 'manager', managerArray ) );
+
+	} catch ( err ) {
+		console.log( err );
+	}
+
+	inquirer
+		.prompt( questions.addEmployee )
+		.then( async ( addEmployeeAnswers ) => {
+
+			const { first_name, last_name, role, manager } = addEmployeeAnswers;
+
+			try {
+				await db.query( queries.insertEmployee, [first_name, last_name, role, manager] );
+
+				console.log( '\x1b[32m', `Added ${first_name} ${last_name} to the database.`, '\x1b[0m' );
+
+				return askForCategory();
+
+			} catch ( err ) {
+				console.log( err );
+			}
+		} );
+};
+
+// Updates an employee's role
+const updateEmployeeRole = async () => {
+	let updateEmployeeRoleQuestions = [];
+
+	try {
+		const employeeTable = await db.query( queries.employeesByRole );
+
+		let employeeArray = employeeTable.map( employee => ( {
+			name: employee.name,
+			value: employee.id
+		} ) );
+
+		updateEmployeeRoleQuestions.push( constructListQuestion( 'Choose an employee to update thier role', 'employee', employeeArray ) );
+
+	} catch ( err ) {
+		console.log( err );
+	}
+
+	try {
+		const roleTable = await db.query( queries.roles );
+
+		let roleArray = roleTable.map( role => ( {
+			name: role.title,
+			value: role.id
+		} ) );
+
+		updateEmployeeRoleQuestions.push( constructListQuestion( 'Choose a new role for this employee', 'role', roleArray ) );
+
+	} catch ( err ) {
+		console.log( err );
+	}
+
+	inquirer
+		.prompt( updateEmployeeRoleQuestions )
+		.then( async ( updateEmployeeAnswers ) => {
+
+			const { role, employee } = updateEmployeeAnswers;
+
+			try {
+				await db.query( queries.updateEmployee( 'role' ), [role, employee] );
+
+				console.log( '\x1b[32m', 'Updated employee\'s role in the database.', '\x1b[0m' );
+
+				return askForCategory();
+
+			} catch ( err ) {
+				console.log( err );
+			}
+		} );
+};
+
+// Updates an employee's manager
+const updateEmployeeManager = async () => {
+	let updateEmployeeManagerQuestions = [];
+
+	try {
+		const employeeTable = await db.query( queries.employeesByRole );
+
+		let employeeArray = employeeTable.map( employee => ( {
+			name: employee.name,
+			value: employee.id
+		} ) );
+
+		updateEmployeeManagerQuestions.push( constructListQuestion( 'Choose an employee to update thier manager', 'employee', employeeArray ) );
+		updateEmployeeManagerQuestions.push( constructListQuestion( 'Choose a new manager for this employee. (Choosing the same employee sets the manager to no one)', 'manager', employeeArray ) );
+
+	} catch ( err ) {
+		console.log( err );
+	}
+
+	inquirer
+		.prompt( updateEmployeeManagerQuestions )
+		.then( async ( updateEmployeeAnswers ) => {
+
+			let { manager, employee } = updateEmployeeAnswers;
+
+			if( manager === employee ) {
+				manager = null;
+			}
+
+			try {
+				await db.query( queries.updateEmployee( 'manager' ), [manager, employee] );
+
+				console.log( '\x1b[32m', 'Updated employee\'s manager in the database.', '\x1b[0m' );
+
+				return askForCategory();
+
+			} catch ( err ) {
+				console.log( err );
+			}
+		} );
+};
+
+// Remove an employee
+const deleteEmployee = async () => {
+	let chooseEmployeeQuestions = [];
+
+	try {
+		const table = await db.query( queries.employeesByRole );
+
+		let employeeArray = table.map( employee => ( {
+			name: employee.name,
+			value: employee.id
+		} ) );
+
+		chooseEmployeeQuestions.push( constructListQuestion( 'Choose an employee to delete', 'employee', employeeArray ) );
+
+	} catch ( err ) {
+		console.log( err );
+	}
+
+	inquirer
+		.prompt( chooseEmployeeQuestions )
+		.then( async ( choosenEmployee ) => {
+
+			const employee = choosenEmployee.employee;
+
+			try {
+				await db.query( queries.delete( 'employee' ), employee );
+
+				console.log( '\x1b[33m', 'Deleted employee from the database.', '\x1b[0m' );
+
+				return askForCategory();
+
+			} catch ( err ) {
+				console.log( err );
+			}
+		} );
+};
+
+// Ask the user for what action they want to take with employees
+const askForEmployeeAction = () => {
+	inquirer
+		.prompt( questions.employee )
+		.then( ( employeeAnswer ) => {
+
+			switch( employeeAnswer.action ) {
+
+			case 'View All Employees':
+				return selectEmployeeTable();
+
+			case 'View Employees by Manager':
+				return selectEmployeeManagerTable();
+
+			case 'View Employees by Department':
+				return selectEmployeeDepartmentTable();
+
+			case 'Add An Employee':
+				return addEmployee();
+
+			case 'Update An Employee\'s Role':
+				return updateEmployeeRole();
+
+			case 'Update An Employee\'s Manager':
+				return updateEmployeeManager();
+
+			case 'Delete An Employee':
+				return deleteEmployee();
+			}
+		} );
+};
+
+// Ask the user what category of data they want to work with
+const askForCategory = () => {
+	inquirer
+		.prompt( questions.category )
+		.then( ( categoryAnswer ) => {
+
+			switch( categoryAnswer.category ) {
+
+			case 'Departments':
+				return askForDepartmentAction();
+
+			case 'Roles':
+				return askForRoleAction();
+
+			case 'Employees':
+				return askForEmployeeAction();
+
+			case 'Quit':
+				console.log( '\x1b[34m', 'Goodbye!', '\x1b[0m' );
+				return db.end();
+			}
+		} );
+};
+
+askForCategory();
